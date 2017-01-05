417882ce1b0864eb08c1be21c0a0d80ca8407a9f
Ilia Pochinskii <zorkiypnz@yandex.ru>
2017-01-05 22:50:48 +0100

    refactored "Calculate Variance" a little

--- js/5kyu/calculate_variance.js
+++ js/5kyu/calculate_variance.js
@@ -1,9 +1,9 @@
 const sum = (numbers) => numbers.reduce((memo, current) => memo + current, 0);
 
-const mean = (numbers, length) => sum(numbers) / length;
+const mean = (numbers, length = null) => sum(numbers) / ( length || numbers.length);
 
 const variance = (numbers) => {
-  const m = mean(numbers, numbers.length);
+  const m = mean(numbers);
   const subs = numbers.map((el) => Math.pow(el - m, 2));
   return mean(subs, numbers.length);
 };
