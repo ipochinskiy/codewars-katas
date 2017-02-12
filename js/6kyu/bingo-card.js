d2e1aedbc2fdf81a3eadef99da606a4c94b779d3
Ilia Pochinskii <zorkiypnz@yandex.ru>
2017-02-12 20:47:44 +0100

    simlified solution and description for kata "Bingo Card"

--- js/6kyu/bingo-card.js
+++ js/6kyu/bingo-card.js
@@ -7,42 +7,30 @@ const COLUMNS = [
   { min: 61, max: 76 },
 ];
 
-const isSpecial = (row, column) => (row === 2) && (column === 2);
+const isSpecial = (column) => column === 2;
 
-const makeRandomGenerator = ({ min, max }) => () => {
-  const minInt = Math.ceil(min);
-  const maxInt = Math.floor(max);
-  return Math.floor(Math.random() * (maxInt - minInt)) + minInt;
+const makeGenerator = ({ min, max }) =>
+  () => Math.floor(Math.random() * (max - min)) + min;
+
+const getUniqueInt = (excludes, generator) => {
+  const n = generator();
+  return excludes.includes(n) ?
+    getUniqueInt(excludes, generator) :
+    n;
 };
 
-
-const initUniqueGenerator = () => {
-  const numbers = [];
-  return function getUniqueInt(generate) {
-    const n = generate();
-    if (numbers.includes(n)) {
-      return getUniqueInt(generate);
-    }
-    numbers.push(n);
+const generateColumn = (generator, amount) => {
+  const excludes = [];
+  return Array(amount).fill().map(() => {
+    const n = getUniqueInt(excludes, generator);
+    excludes.push(n);
     return n;
-  };
+  });
 };
 
-const getCard = () => {
-  const getUniqueInt = initUniqueGenerator();
-
-  return Array(COLUMNS.length).fill()
-    .map((_, column) => {
-      const getRandomWithinRange = makeRandomGenerator(COLUMNS[column]);
-
-      return Array(COLUMNS.length).fill()
-        .map((_, row) => {
-          if (isSpecial(row, column)) {
-            return null;
-          }
-          return getUniqueInt(getRandomWithinRange);
-        })
-        .filter((number) => !!number)
-        .map((number) => NAME[column] + number);
-    }).reduce((m, c) => m.concat(c), []);
-};
+const getCard = () => Array(COLUMNS.length).fill()
+  .map((_, column) => generateColumn(
+    makeGenerator(COLUMNS[column]),
+    isSpecial(column) ? 4 : 5
+  ).map((number) => NAME[column] + number))
+  .reduce((m, c) => m.concat(c), []);
