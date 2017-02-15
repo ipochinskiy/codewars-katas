6ecfd32f7e66a6a73660568b82b1105cc710554f
Ilia Pochinskii <zorkiypnz@yandex.ru>
2017-02-15 23:15:21 +0100

    refactored solution for kata "String incrementer"

--- js/6kyu/string-incrementer.js
+++ js/6kyu/string-incrementer.js
@@ -1,15 +1,5 @@
-const ENDS_WITH_DIGITS_REGEX = /\d*$/;
-
-const incrementString = (textWithDigits = '') => {
-
-  const text = textWithDigits.split(ENDS_WITH_DIGITS_REGEX)[0] || '';
-  const oldDigits = textWithDigits.match(ENDS_WITH_DIGITS_REGEX)[0] || '';
-  const newDigits = String(Number(oldDigits) + 1);
-
-  const zerosCount = oldDigits.length - newDigits.length;
-  let zeros = zerosCount > 0 ?
-    Array(zerosCount).fill('0').join('') :
-    '';
-
-  return text + zeros + newDigits;
+const incrementString = (strng = '') => {
+  const m = strng.match(/^(\w*?)(\d*)$/);
+  const next = String(Number('0' + m[2], 10) + 1);
+  return m[1] + m[2].slice(0, -next.length) + next;
 };
